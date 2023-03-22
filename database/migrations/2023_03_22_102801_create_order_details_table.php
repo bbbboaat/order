ff4b3f584bde077b54ff15bcccb4f8db34c67775
bbbboaat <64531509+bbbboaat@users.noreply.github.com>
2023-03-22 17:36:16 +0700

    setdbOrder

--- database/migrations/2023_03_22_102801_create_order_details_table.php
+++ database/migrations/2023_03_22_102801_create_order_details_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
+            $table->integer('order_id');
+            $table->integer('product_id');
+            $table->string('product_name');
+            $table->decimal('price');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
