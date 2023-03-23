1e0c5e149024a416ae075f59b0aa7156d5852212
bbbboaat <64531509+bbbboaat@users.noreply.github.com>
2023-03-23 15:18:47 +0700

    userPermiss

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
 {
     function __construct()
     {
-        $this->middleware('admin');
+        $this->middleware('admin')->except(['index', 'search']);
     }
     public function index(Request $request)
     {
